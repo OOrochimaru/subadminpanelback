b6efbaadaa4a4663e0b97fd78490afbd90b40567
ryam <ryambudha@gmail.com>
2018-11-06 12:54:54 +0530

    social login

--- controllers/socialLoginController.js
+++ controllers/socialLoginController.js
@@ -1,6 +1,7 @@
 var database = require('../config/database');
 var User = require('../model/user');
-var jwt = require('jsonwebtoken')
+var jwt = require('jsonwebtoken');
+var config = require('../config/database');
 
 module.exports.login = function(obj, callback){
     User.findOne({email: obj.email}).exec(function(err, user){
@@ -11,7 +12,7 @@ module.exports.login = function(obj, callback){
             const user = new User({
                 facebook_id : obj.facebook_id,
                 google_id : obj.google_id,
-                linkedIN_id : obj.linkedin_id,
+                linkedIN_id : obj.linkedIN_id,
                 twitter_id : obj.twitter_id,
                 name : obj.profile.displayName,
                 email : obj.email,
@@ -24,10 +25,30 @@ module.exports.login = function(obj, callback){
                 if (err) {
                     callback(err, null);
                 }else{
+                    let token = jwt.sign({id : user._id}, config.secret, {expiresIn: 86400});
+                    User.findByIdAndUpdate({_id: user._id }, {$set: {token : token}})
+                    .exec(function(err, user){
+                        if (err) {
+                            callback(err, null);
+                        }else{
+                            callback(null, token);
+                        }
 
+                    })
                 }
 
             })
+        }else{ // if the user already exist in the database
+            let token = jwt.sign({id : user._id}, config.secret, {expiresIn: 86400});
+            let socialFlag = false;
+            
+            if (user.facebook_id === obj.facebook_id ||
+                user.google_id === obj.google_id ||
+                user.twitter_id === obj.twitter_id ||
+                user.linkedIN_id === obj.linkedIN_id      
+            ) {
+                
+            }
         }
     })    
 }
